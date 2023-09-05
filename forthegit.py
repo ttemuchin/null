3c961734329eacbee17c01b7d5820c3176281b33
Artem <mokoseev.artem@gmail.com>
2023-09-05 18:26:41 +0300

    add some

--- forthegit.py
+++ forthegit.py
@@ -2,4 +2,11 @@ print("\nInsert values for the triangle, please: \n")
 a = int(input())
 b = int(input())
 c = int(input())
-print(a+b+c)
+if (a<=0) or (b<=0) or (c<=0):
+    print("Try values > 0")
+else:
+    if (a+b>c) or (a+c>b) or (c+b>a):
+        print("exists")
+    else:
+        print("sry, but such triangle doesn't exist")
+
