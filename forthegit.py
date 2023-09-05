72a2f99e0856f9fb7f18c79275c393581f62c08e
Artem <mokoseev.artem@gmail.com>
2023-09-05 18:43:30 +0300

    function type

--- forthegit.py
+++ forthegit.py
@@ -1,3 +1,11 @@
+def type(a, b, c):
+    if a == b and b == c:
+        print('This triangle is equilateral')
+    elif a == b or a == c or b == c:
+        print('This triangle is isosceles')
+    else: 
+        print("This triangle is versatile")
+
 print("\nInsert values for the triangle, please: \n")
 a = int(input())
 b = int(input())
@@ -5,8 +13,8 @@ c = int(input())
 if (a<=0) or (b<=0) or (c<=0):
     print("Try values > 0")
 else:
-    if (a+b>c) or (a+c>b) or (c+b>a):
-        print("exists")
+    if (a+b>c) and (a+c>b) and (c+b>a):
+        type(a, b, c)
     else:
         print("sry, but such triangle doesn't exist")
 
